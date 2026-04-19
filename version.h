d134946b5a05df96621ab9a5bfad0d975cae5000
Kevgor <kevgor2003@hotmail.com>
2026-04-18 23:52:05 -0700

    version

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 #pragma once
-#define FW_VERSION "git-e3abea4"
+#define FW_VERSION "git-0d16e07"
 #define FW_COMPILED_DATE "2026-04-18"
-#define FW_COMPILED_TIME "14:22:36"
+#define FW_COMPILED_TIME "23:51:56"
